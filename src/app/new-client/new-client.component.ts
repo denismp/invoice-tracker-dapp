80f308cad80c0a4e5f96441ffdae03e216da2c19
Denis Putnam <denisputnam@verizon.net>
2020-07-14 18:25:43 -0400

    Added more validation for new-client

--- src/app/new-client/new-client.component.ts
+++ src/app/new-client/new-client.component.ts
@@ -10,13 +10,44 @@ export class NewClientComponent {
   addressForm = this.fb.group({
     name: [null, Validators.required],
     lastName: [null, Validators.required],
-    address: [null, Validators.required],
+    address: [null, [Validators.required, Validators.minLength(42), Validators.maxLength(42)]],
   });
 
 
-  constructor(private fb: FormBuilder) {}
+  constructor(private fb: FormBuilder) { }
 
   onSubmit() {
-    alert('Thanks!');
+    let address: string;
+    let name: string;
+    let error: boolean = false;
+    if( this.addressForm.get('name').value !== '') {
+      name = this.addressForm.get('name').value;
+    } else {
+      error = true;
+    }
+    if (this.addressForm.get('address').value !== '') {
+      address = this.addressForm.get('address').value;
+    } else {
+      error = true;
+    }
+
+    address = this.addressForm.get('address').value;
+    console.log('length=' + address.length);
+    let res: string = "Thank you."
+    if (error === true) {
+      res = "The data you entered is invalid."
+    }
+    alert(res);
   }
+
+  getAddressErrorMessage(): string {
+    if (this.addressForm.get('address').hasError('required')) {
+      return 'You must enter a 20 byte hex address';
+    }
+    if (this.addressForm.get('address').hasError('address')) {
+      return 'Not a valid 20 byte hex address';
+    }
+    return '';
+  }
+
 }

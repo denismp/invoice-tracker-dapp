e5b23474cd3150edaf09c826395e4b55ab91d9fd
Denis Putnam <denisputnam@verizon.net>
2020-07-15 11:39:51 -0400

    Added check for valid hex address value.

--- src/app/new-client/new-client.component.ts
+++ src/app/new-client/new-client.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { FormBuilder, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-new-client',
@@ -10,7 +10,8 @@ export class NewClientComponent {
   addressForm = this.fb.group({
     name: [null, Validators.required],
     lastName: [null, Validators.required],
-    address: [null, [Validators.required, Validators.minLength(42), Validators.maxLength(42)]],
+    address: [null, [Validators.required, Validators.minLength(42), Validators.maxLength(42), this.isNotHex.bind(this)]]
+    //address: [null, [Validators.required, Validators.minLength(42), Validators.maxLength(42)]],
   });
 
 
@@ -20,13 +21,16 @@ export class NewClientComponent {
     let address: string;
     let name: string;
     let error: boolean = false;
-    if( this.addressForm.get('name').value !== '') {
+    if (this.addressForm.get('name').value !== '') {
       name = this.addressForm.get('name').value;
     } else {
       error = true;
     }
     if (this.addressForm.get('address').value !== '') {
       address = this.addressForm.get('address').value;
+      if (this.isValidHexString(address) === false ) {
+        error = true;
+      }
     } else {
       error = true;
     }
@@ -40,12 +44,50 @@ export class NewClientComponent {
     alert(res);
   }
 
-  getAddressErrorMessage(): string {
-    if (this.addressForm.get('address').hasError('required')) {
-      return 'You must enter a 20 byte hex address';
+  isValidHexString(str: string): boolean {
+    if (str === null) {
+      return false;
     }
-    if (this.addressForm.get('address').hasError('address')) {
-      return 'Not a valid 20 byte hex address';
+    let rVal: boolean = true;
+    const upperStr: string = str.toUpperCase();
+    const hexDidgits: string = "XABCDEF0123456789"; // deals with leading 0x
+    for (let i = 0; i < upperStr.length; i++) {
+      let found = false;
+      for (let j = 0; j < hexDidgits.length; j++) {
+        if (upperStr.charAt(i) === hexDidgits.charAt(j)) {
+          found = true;
+          break;
+        }
+      }
+      if (found === false) {
+        rVal = false;
+        break;
+      }
+    }
+    console.log("isValidHexString()=", rVal);
+    return rVal;
+  }
+
+  isNotHex(control: FormControl): { [s: string]: boolean } {
+    if (this.isValidHexString(control.value) !== true) {
+      return { 'notHex': true };
+    }
+    return null;
+  }
+
+  getAddressErrorMessage(): string {
+    console.log('getAddressErrorMessage(): called...');
+    if (this.addressForm.get('address').hasError('required')) {
+      console.log('getAddressErrorMessage(): DEBUG1');
+      return 'No value.  You must enter a 42 byte hex address starting with 0x';
+    }
+    if (this.addressForm.get('address').hasError('notHex')) {
+      console.log('getAddressErrorMessage(): DEBUG2');
+      return 'Invalid hex value. You must enter a 42 byte hex address starting with 0x';
+    }
+    if (this.addressForm.get('address').invalid) {
+      console.log('getAddressErrorMessage(): DEBUG3');
+      return 'Incorrect address length.  You must enter a 42 byte hex address starting with 0x';
     }
     return '';
   }

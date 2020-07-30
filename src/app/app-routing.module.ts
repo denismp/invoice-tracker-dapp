522646ee5fa4668cca1f9eafae4a94eb4128a1ec
Denis Putnam <denisputnam@verizon.net>
2020-07-30 17:47:27 -0400

    Modified routing and navigation for the User Login.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,9 +5,11 @@ import { ClientListComponent } from './client-list/client-list.component';
 import { NewInvoiceComponent } from './new-invoice/new-invoice.component';
 import { UpdateInvoiceComponent } from './update-invoice/update-invoice.component';
 import { InvoiceListComponent } from './invoice-list/invoice-list.component';
+import { UserComponent } from './user/user.component';
 
 
 const routes: Routes = [
+  { path: 'user', component: UserComponent },
   { path: 'new-client', component: NewClientComponent },
   { path: 'client-list', component: ClientListComponent },
   { path: 'new-invoice', component: NewInvoiceComponent },

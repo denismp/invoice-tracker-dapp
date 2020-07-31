2409ae9a5f4f4aac898a031d7ec40f42961927a2
Denis Putnam <denisputnam@verizon.net>
2020-07-31 18:57:10 -0400

    Got one user created.  Still need more work.

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { FormBuilder, Validators, FormControl } from '@angular/forms';
 import { UserService } from '../services/user.service';
+import { Web3Service } from '../services/web3.service';
 
 @Component({
   selector: 'app-user',
@@ -9,86 +10,107 @@ import { UserService } from '../services/user.service';
 })
 export class UserComponent {
   addressForm = this.fb.group({
-    company: null,
-    firstName: [null, Validators.required],
-    lastName: [null, Validators.required],
-    address: [null, Validators.required],
-    address2: null,
-    city: [null, Validators.required],
-    state: [null, Validators.required],
-    postalCode: [null, Validators.compose([
-      Validators.required, Validators.minLength(5), Validators.maxLength(5)])
-    ],
-    shipping: ['free', Validators.required]
+    name: [null, Validators.required],
+    address: [null, [Validators.required, Validators.minLength(42), Validators.maxLength(42), this.isNotHex.bind(this)]],
+    pwd: [null, [Validators.required, Validators.minLength(8)]]
+    //address: [null, [Validators.required, Validators.minLength(42), Validators.maxLength(42)]],
   });
 
-  hasUnitNumber = false;
+  submitting: boolean = false;
 
-  states = [
-    {name: 'Alabama', abbreviation: 'AL'},
-    {name: 'Alaska', abbreviation: 'AK'},
-    {name: 'American Samoa', abbreviation: 'AS'},
-    {name: 'Arizona', abbreviation: 'AZ'},
-    {name: 'Arkansas', abbreviation: 'AR'},
-    {name: 'California', abbreviation: 'CA'},
-    {name: 'Colorado', abbreviation: 'CO'},
-    {name: 'Connecticut', abbreviation: 'CT'},
-    {name: 'Delaware', abbreviation: 'DE'},
-    {name: 'District Of Columbia', abbreviation: 'DC'},
-    {name: 'Federated States Of Micronesia', abbreviation: 'FM'},
-    {name: 'Florida', abbreviation: 'FL'},
-    {name: 'Georgia', abbreviation: 'GA'},
-    {name: 'Guam', abbreviation: 'GU'},
-    {name: 'Hawaii', abbreviation: 'HI'},
-    {name: 'Idaho', abbreviation: 'ID'},
-    {name: 'Illinois', abbreviation: 'IL'},
-    {name: 'Indiana', abbreviation: 'IN'},
-    {name: 'Iowa', abbreviation: 'IA'},
-    {name: 'Kansas', abbreviation: 'KS'},
-    {name: 'Kentucky', abbreviation: 'KY'},
-    {name: 'Louisiana', abbreviation: 'LA'},
-    {name: 'Maine', abbreviation: 'ME'},
-    {name: 'Marshall Islands', abbreviation: 'MH'},
-    {name: 'Maryland', abbreviation: 'MD'},
-    {name: 'Massachusetts', abbreviation: 'MA'},
-    {name: 'Michigan', abbreviation: 'MI'},
-    {name: 'Minnesota', abbreviation: 'MN'},
-    {name: 'Mississippi', abbreviation: 'MS'},
-    {name: 'Missouri', abbreviation: 'MO'},
-    {name: 'Montana', abbreviation: 'MT'},
-    {name: 'Nebraska', abbreviation: 'NE'},
-    {name: 'Nevada', abbreviation: 'NV'},
-    {name: 'New Hampshire', abbreviation: 'NH'},
-    {name: 'New Jersey', abbreviation: 'NJ'},
-    {name: 'New Mexico', abbreviation: 'NM'},
-    {name: 'New York', abbreviation: 'NY'},
-    {name: 'North Carolina', abbreviation: 'NC'},
-    {name: 'North Dakota', abbreviation: 'ND'},
-    {name: 'Northern Mariana Islands', abbreviation: 'MP'},
-    {name: 'Ohio', abbreviation: 'OH'},
-    {name: 'Oklahoma', abbreviation: 'OK'},
-    {name: 'Oregon', abbreviation: 'OR'},
-    {name: 'Palau', abbreviation: 'PW'},
-    {name: 'Pennsylvania', abbreviation: 'PA'},
-    {name: 'Puerto Rico', abbreviation: 'PR'},
-    {name: 'Rhode Island', abbreviation: 'RI'},
-    {name: 'South Carolina', abbreviation: 'SC'},
-    {name: 'South Dakota', abbreviation: 'SD'},
-    {name: 'Tennessee', abbreviation: 'TN'},
-    {name: 'Texas', abbreviation: 'TX'},
-    {name: 'Utah', abbreviation: 'UT'},
-    {name: 'Vermont', abbreviation: 'VT'},
-    {name: 'Virgin Islands', abbreviation: 'VI'},
-    {name: 'Virginia', abbreviation: 'VA'},
-    {name: 'Washington', abbreviation: 'WA'},
-    {name: 'West Virginia', abbreviation: 'WV'},
-    {name: 'Wisconsin', abbreviation: 'WI'},
-    {name: 'Wyoming', abbreviation: 'WY'}
-  ];
-
-  constructor(private fb: FormBuilder, private userService: UserService) {}
+  constructor(private fb: FormBuilder, private userService: UserService, private web3Service: Web3Service) { }
 
   onSubmit() {
-    alert('Thanks!');
+    //let userAddress: string = this.userService.userAddress;
+    let address: string = this.addressForm.get('address').value;
+    let name: string = this.addressForm.get('name').value;
+    let pwd: string = this.addressForm.get('pwd').value;
+    let error: boolean = false;
+    // if (userAddress === "undefined") {
+    //   userAddress = address;
+    //   this.userService.userAddress = address;
+    // }
+    // TODO Deal with existing user.  Query the contract to see if the user exists.
+    if (name === "undefined") {
+      error = true;
+    }
+    if (address === "undefined") {
+      error = true;
+    }
+    if (pwd === "undefined") {
+      error = true;
+    }
+
+    let rString: string = "Thank you."
+    if (error === true) {
+      rString = "The data you entered is invalid."
+      alert(rString);
+    } else {
+      this.submitting = true;
+      this.userService.createUser(address, name, pwd)
+        .then(res => {
+          this.submitting = false;
+          console.log('UserComponent.onSubmit(): res: ', res);
+          if (this.userService.success === true) {
+            let myData: string = "transactionHash=" + res.transactionHash + " blockHash=" + res.blockHash + " blockNumber=" + res.blockNumber;
+            alert('Successfully added ' + name + " " + myData);
+          } else {
+            alert('Add of ' + name + ' failed');
+          }
+        })
+        .catch(err => {
+          this.submitting = false;
+          console.log('UserComponent.onSubmit(): err: ', err);
+          alert('Submit failed.');
+        });
+    }
+  }
+
+  isValidHexString(str: string): boolean {
+    if (str === null) {
+      return false;
+    }
+    let rVal: boolean = true;
+    const upperStr: string = str.toUpperCase();
+    const hexDidgits: string = "XABCDEF0123456789"; // deals with leading 0x
+    for (let i = 0; i < upperStr.length; i++) {
+      let found = false;
+      for (let j = 0; j < hexDidgits.length; j++) {
+        if (upperStr.charAt(i) === hexDidgits.charAt(j)) {
+          found = true;
+          break;
+        }
+      }
+      if (found === false) {
+        rVal = false;
+        break;
+      }
+    }
+    console.log("isValidHexString()=", rVal);
+    return rVal;
+  }
+
+  isNotHex(control: FormControl): { [s: string]: boolean } {
+    if (this.isValidHexString(control.value) !== true) {
+      return { 'notHex': true };
+    }
+    return null;
+  }
+
+  getAddressErrorMessage(): string {
+    console.log('getAddressErrorMessage(): called...');
+    if (this.addressForm.get('address').hasError('required')) {
+      console.log('getAddressErrorMessage(): DEBUG1');
+      return 'No value.  You must enter a 42 byte hex address starting with 0x';
+    }
+    if (this.addressForm.get('address').hasError('notHex')) {
+      console.log('getAddressErrorMessage(): DEBUG2');
+      return 'Invalid hex value. You must enter a 42 byte hex address starting with 0x';
+    }
+    if (this.addressForm.get('address').invalid) {
+      console.log('getAddressErrorMessage(): DEBUG3');
+      return 'Incorrect address length.  You must enter a 42 byte hex address starting with 0x';
+    }
+    return '';
   }
 }
